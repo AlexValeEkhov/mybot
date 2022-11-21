36e7d5129800be546b8662f29ca6877157b1a094
Alexander Ekhov <alex-vale@mail.ru>
2022-11-21 17:38:15 +0300

    Создаю моего первого бота

---
+++ bot.py
@@ -0,0 +1,29 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import settings
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+def greet_user(update, context):
+    print('Вызван /start')
+    update.message.reply_text('Привет, пользователь! Ты вызвал команду старт')
+
+def talk_to_me(update, context):
+    text = update.message.text
+    print(text)
+    update.message.reply_text(text)
+
+
+def main():
+    mybot = Updater(settings.API_KEY, use_context=True)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    logging.info("Bot started")
+    mybot.start_polling()
+    mybot.idle()
+
+if __name__ == "__main__":
+    main()
